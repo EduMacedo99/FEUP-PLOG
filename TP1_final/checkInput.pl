140c015a5f6897d312ee9738177384531a7cde58
unknown <eduardojcmacedo@gmail.com>
2019-11-06 22:00:04 +0000

    progress

--- TP1_final/checkInput.pl
+++ TP1_final/checkInput.pl
@@ -14,34 +14,40 @@ checkRow('h', 8).
 
 checkRow(_Row, NewRow):-
     write('Invalid Row!\n'),
-    readRow(Input),
+    write('> Row?\n'),
+    read(Input),
     checkRow(Input, NewRow).
 
 
 askColumn(NewColumn) :-
     write('> Column?\n'),
     read(Column),
-    checkRow(Column, NewColumn).
+    checkColumn(Column, NewColumn).
 
-checkColumn('1', 1).
-
-checkColumn('2', 2).
-checkColumn('3', 3).
-checkColumn('4', 4).
-checkColumn('5', 5).
-checkColumn('6', 6).
-checkColumn('7', 7).
-checkColumn('8', 8).
+checkColumn(1, 1).
+checkColumn(2, 2).
+checkColumn(3, 3).
+checkColumn(4, 4).
+checkColumn(5, 5).
+checkColumn(6, 6).
+checkColumn(7, 7).
+checkColumn(8, 8).
 
 checkColumn(_Column, NewColumn):-
     write('Invalid Column!\n'),
-    readColumn(Input),
+    read(Input),
     checkColumn(Input, NewColumn).
 
-checkWhiteCoord(Row, Column, Board):-
+checkWhiteCoord(Row, Column, Board, Peca):-
     getPeca(Row, Column, Board, Peca),
     (   Peca \= white ->
-    askCoords(Row, Column),
-    write('not white!\n')
-    ;   write('niceee\n')
+    write('not white!\n'), 
+    askCoords(NewRow, NewColumn),
+    checkWhiteCoord(NewRow, NewColumn, Board)
+    ;   write('Valid answer!\n')
     ).
+
+askBlocks(Number):-
+    write('\nHow many block do you wish to move?'),
+    read(Number).
+
--- TP1_final/logic.pl
+++ TP1_final/logic.pl
@@ -1,14 +1,17 @@
 game(Player1, Player2) :-
       tabuleiroInicial(Board), %fazer isto random?
       display_game(Board),
+      %dentro do white turn
       askCoords(NewRow, NewColumn),
-      checkWhiteCoord(NewRow, NewColumn, Board),
+      checkWhiteCoord(NewRow, NewColumn, Board, Peca),
+      askBlocks(Number),
+      makeMovement(NewRow, NewColumn, Peca, Number, Board, NewBoard),
+    %   display_game(NewBoard),
+      %fim de white turn
       write('ok\n').
       %mainLoop(WorkersBoard, Player1, Player2).
 
 %mainLoop(Board, Player1, Player2):-
-
-
 
 askCoords(NewRow, NewColumn):-
     askRow(NewRow),
@@ -17,11 +20,8 @@ askCoords(NewRow, NewColumn):-
     nl.
 
 getPeca(NLinha, NColuna, Board, Peca):-
-    nth1(NLinha, Board, NColuna),
-    nth1(NColuna, Board, Peca).
-
-
-
+    nth1(NLinha, Board, Coluna),
+    nth1(NColuna, Coluna, Peca).
 
 setPeca(NLinha, NColuna, Peca, TabIn, TabOut):-
     setLinha(NLinha, NColuna, Peca, TabIn, TabOut).
@@ -40,3 +40,12 @@ setColuna(N, Peca, [X | Resto], [X | Mais]):-
     N > 1,
     Next is N - 1,
     setColuna(Next, Peca, Resto, Mais).
+
+makeMovement(Row, Column, Peca, steps, Board, NewBoard):-
+    write('entrou\n'),
+    (Column == 1 ->
+    write('column 1!\n')
+    % setPeca(Row, Column + steps, Peca, Board, NewBoard)
+    ;
+    write('not col 1\n')
+    ).
